43c2463eb0c60fbffe4a3aa305a6823ceaae41ef
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-05-17 15:01:38 -0700

    logout funtion added on nav bar, fix to be positioned on the right

--- Angular_frontend/src/app/nav-bar/nav-bar.component.ts
+++ Angular_frontend/src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  logged!: boolean;
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  ngDoCheck(): void {
+    if (localStorage.getItem("jwt")) {
+      this.logged = true;
+    }
+  }
+
+  logout(): void {
+    localStorage.removeItem("jwt");
+    localStorage.removeItem("userId");
+    localStorage.removeItem("user_role");
+    localStorage.removeItem("username");
+    this.logged = false;
+    this.router.navigate(['/login'])
+  }
 }
